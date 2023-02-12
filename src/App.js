9815fa110f26ec7b731bb8a6d99c525665f0bc66
Nirob Hasan <nirob427417@gmail.com>
2023-02-12 11:36:08 +0600

    react router & header, footer setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './Routes/Router/Router';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hi </h1>
+    <div>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
